b4e09737a7ba3222db89915311b59f8fd9889ab2
hi-lufz1 <latifufauzi@gmail.com>
2025-05-21 13:25:20 +0700

    membuat dan menambahkan widget RecomendsPlants ke Body

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox..dart';
+import 'package:plant_app/screens/home/components/recomend_plants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
 
 class Body extends StatelessWidget {
@@ -13,6 +14,7 @@ class Body extends StatelessWidget {
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
           SizedBox(height: kDefaultPadding),
         ],
       ),
