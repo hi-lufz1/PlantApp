a83301cf4f4637cc377c474afc430a8f18ac05c5
hi-lufz1 <latifufauzi@gmail.com>
2025-05-21 13:47:13 +0700

    menambahkan widget FeaturedPlants dan memperbarui Body untuk menampilkan Featured Plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/featurred_plants.dart';
 import 'package:plant_app/screens/home/components/header_with_searchbox..dart';
 import 'package:plant_app/screens/home/components/recomend_plants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_bbtn.dart';
@@ -15,6 +16,8 @@ class Body extends StatelessWidget {
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recomended", press: () {}),
           RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
           SizedBox(height: kDefaultPadding),
         ],
       ),
---
+++ lib/screens/home/components/featurred_plants.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: const <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: _dummyPress,
+          ),
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: _dummyPress,
+          ),
+        ],
+      ),
+    );
+  }
+
+  static void _dummyPress() {}
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    super.key,
+    required this.image,
+    required this.press,
+  });
+
+  final String image;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: const EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
+  }
+}
