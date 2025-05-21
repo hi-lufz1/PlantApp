72459c58b90954722cd05550a10a334db6aa57e7
hi-lufz1 <latifufauzi@gmail.com>
2025-05-21 11:49:22 +0700

    menambahkan Body widget ke HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/screens/home/components/body.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
      return Scaffold(
       appBar: buildAppBar(),
+      body: Body(),
     );
   }
 
