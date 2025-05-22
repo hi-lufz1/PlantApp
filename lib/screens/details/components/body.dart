c64ea8e45ccd708babf942154de5416fb74732be
hi-lufz1 <latifufauzi@gmail.com>
2025-05-22 11:00:05 +0700

    menambahkan image dan icons di body

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'images_and_icons.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(child: Column(children: <Widget>[
-        ],
-      ));
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[ImagesAndIcons(size: size)]),
+    );
   }
 }
