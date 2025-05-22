5fd0b4f98b8cf6c82f959658370c2d20716d6f0d
hi-lufz1 <latifufauzi@gmail.com>
2025-05-22 22:11:12 +0700

    menambahkan map state

--- lib/bloc/map_state.dart
+++ lib/bloc/map_state.dart
@@ -1,6 +1,38 @@
-part of 'map_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-@immutable
-sealed class MapState {}
+class MapState extends Equatable {
+  final CameraPosition? initialCamera;
+  final Marker? pickedMarker;
+  final String? pickedAddress;
+  final String? currentAddress;
+  final bool isLoading;
 
-final class MapInitial extends MapState {}
+  const MapState({
+    this.initialCamera,
+    this.pickedMarker,
+    this.pickedAddress,
+    this.currentAddress,
+    this.isLoading = false,
+  });
+
+  MapState copyWith({
+    CameraPosition? initialCamera,
+    Marker? pickedMarker,
+    String? pickedAddress,
+    String? currentAddress,
+    bool? isLoading,
+  }) {
+    return MapState(
+      initialCamera: initialCamera ?? this.initialCamera,
+      pickedMarker: pickedMarker,
+      pickedAddress: pickedAddress,
+      currentAddress: currentAddress ?? this.currentAddress,
+      isLoading: isLoading ?? false,
+    );
+  }
+
+  @override
+  List<Object?> get props =>
+      [initialCamera, pickedMarker, pickedAddress, currentAddress, isLoading];
+}
