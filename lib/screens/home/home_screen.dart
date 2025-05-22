9d71551268872af2b88e4fb2d2759553eda5b46e
hi-lufz1 <latifufauzi@gmail.com>
2025-05-23 00:59:47 +0700

    menambahkan BottomNavBar ke HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/components/bottom_nav_bar.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 
@@ -8,7 +9,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(), body: Body());
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 
   AppBar buildAppBar() {
