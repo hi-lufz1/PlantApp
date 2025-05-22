01992c454f8cf7c669684e616d96b135d01543ed
hi-lufz1 <latifufauzi@gmail.com>
2025-05-22 11:36:41 +0700

    menambahkan tittle and price dan tombol tombol di body details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/details/components/title_and_price.dart';
 
 import 'images_and_icons.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(children: <Widget>[ImagesAndIcons(size: size)]),
+      child: Column(
+        children: <Widget>[
+          ImagesAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                    backgroundColor: kPrimaryColor,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Description"),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
