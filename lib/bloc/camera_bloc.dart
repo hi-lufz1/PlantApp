db3d09645d40372a8839a11c3674b814abc63aa6
hi-lufz1 <latifufauzi@gmail.com>
2025-05-22 23:42:26 +0700

    menambahkan camera bloc yang berisi fungsi fungsi kontrol camera

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,13 +1,85 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'camera_event.dart';
-part 'camera_state.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:camera/camera.dart';
+import 'camera_event.dart';
+import 'camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late final List<CameraDescription> _cameras;
+
   CameraBloc() : super(CameraInitial()) {
-    on<CameraEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitch);
+    on<ToogleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<TapToFocus>(_onTapFocus);
+  }
+
+  Future<void> _onInit(InitializeCamera event, Emitter<CameraState> emit) async {
+    _cameras = await availableCameras();
+    await _setupController(0, emit);
+  }
+
+  Future<void> _onSwitch(SwitchCamera event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final next = (s.selectedIndex + 1) % _cameras.length;
+    await _setupController(next, emit, previous: s);
+  }
+
+  Future<void> _onToggleFlash(ToogleFlash event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final next = s.flashMode == FlashMode.off
+        ? FlashMode.auto
+        : s.flashMode == FlashMode.auto
+            ? FlashMode.always
+            : FlashMode.off;
+    await s.controller.setFlashMode(next);
+    emit(s.copyWith(flashMode: next));
+  }
+
+  Future<void> _onTakePicture(TakePicture event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final file = await s.controller.takePicture();
+    event.onPictureTaken(File(file.path));
+  }
+
+  Future<void> _onTapFocus(TapToFocus event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final relative = Offset(
+      event.position.dx / event.previewSize.width,
+      event.position.dy / event.previewSize.height,
+    );
+    await s.controller.setFocusPoint(relative);
+    await s.controller.setExposurePoint(relative);
+  }
+
+  Future<void> _setupController(int index, Emitter<CameraState> emit, {CameraReady? previous}) async {
+    await previous?.controller.dispose();
+    final controller = CameraController(
+      _cameras[index],
+      ResolutionPreset.medium,
+      enableAudio: false,
+    );
+    await controller.initialize();
+    await controller.setFlashMode(previous?.flashMode ?? FlashMode.off);
+
+    emit(CameraReady(
+      controller: controller,
+      selectedIndex: index,
+      flashMode: previous?.flashMode ?? FlashMode.off,
+    ));
+  }
+
+  @override
+  Future<void> close() async {
+    if (state is CameraReady) {
+      await (state as CameraReady).controller.dispose();
+    }
+    return super.close();
   }
 }
